6d2669b9c5437c4ccef47967f3a76a3b13fada77
ViniciusGR797 <loritoamarelo@outlook.com>
2024-08-22 17:51:13 -0300

    🕍feat: responsive header

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'graph.facebook.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      }
+    ]
+  },
   async redirects() {
     return [
       {
--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -1,36 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaTwitter, FaLinkedin, FaGoogle, FaPinterestP } from "react-icons/fa";
 import { GoSearch, GoPerson } from "react-icons/go";
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { parseCookies, setCookie, destroyCookie } from "nookies";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Header: React.FC = () => {
   const router = useRouter();
+
+  const [user, setUser] = useState<any>(null);
+  const [showLogout, setShowLogout] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleLogout = () => {
+    destroyCookie(null, "@auth.token", {
+      path: "/",
+    });
+
+    destroyCookie(null, "@auth.user", {
+      path: "/",
+    });
+    router.push("/");
+  };
+
+  useEffect(() => {
+    const cookies = parseCookies();
+    const userCookie = cookies["@auth.user"]
+      ? JSON.parse(cookies["@auth.user"])
+      : null;
+    setUser(userCookie);
+  }, []);
 
   return (
     <header className={styles.header}>
       <div className={styles.contact}>
         <div className={styles.contactLeft}>
           <p>(000) 999-898-999</p>
-          <span>|</span>
+          <span className={styles.separator}>|</span>
           <p>info@trisog.com</p>
         </div>
         <div className={styles.contactRight}>
           <a href="https://x.com" target="_blank" rel="noopener noreferrer">
             <FaTwitter className={styles.iconSocialMedia} />
           </a>
-          <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaLinkedin className={styles.iconSocialMedia} />
           </a>
-          <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaGoogle className={styles.iconSocialMedia} />
           </a>
-          <a href="https://pinterest.com/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://pinterest.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaPinterestP className={styles.iconSocialMedia} />
           </a>
-          <span>|</span>
+          <span className={styles.separator}>|</span>
           <select className={styles.currencySelect}>
             <option value="EUR">EUR</option>
             <option value="USD">USD</option>
@@ -46,10 +87,16 @@ const Header: React.FC = () => {
             alt="Logo"
             width={120}
             height={35}
+            priority={true}
             className={styles.logo}
           />
         </Link>
-        <nav className={styles.nav}>
+
+        <div className={styles.menuToggle} onClick={toggleMenu}>
+          {menuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
+
+        <nav className={`${styles.nav} ${menuOpen ? styles.menuOpen : ""}`}>
           <ul className={styles.menuBar}>
             <li className={router.pathname === "/home" ? styles.active : ""}>
               <Link href="/home">Home</Link>
@@ -77,15 +124,50 @@ const Header: React.FC = () => {
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
+
+          <div className={styles.rightIcons}>
+            <Link href="/tuors">
+              <GoSearch className={styles.iconMenu} />
+            </Link>
+
+            {user ? (
+              <div
+                className={styles.userProfile}
+                onClick={() => setShowLogout(!showLogout)}
+              >
+                <Image
+                  src={user.photoUrl ? user.photoUrl : "/images/user.svg"}
+                  alt={
+                    user.firstName ? user.firstName : user.email?.split("@")[0]
+                  }
+                  width={35}
+                  height={35}
+                  className={styles.userPhoto}
+                />
+                <span>
+                  {user.firstName ? user.firstName : user.email?.split("@")[0]}
+                </span>
+                {showLogout && (
+                  <button
+                    onClick={handleLogout}
+                    className={styles.logoutButton}
+                  >
+                    Logout
+                  </button>
+                )}
+              </div>
+            ) : (
+              <Link href="/" className={styles.loginSignup}>
+                <GoPerson className={styles.iconMenu} />
+                <span>Login / SignUp</span>
+              </Link>
+            )}
+          </div>
         </nav>
 
-        <div className={styles.rightIcons}>
-          <GoSearch className={styles.iconMenu} />
-          <Link href="/" className={styles.loginSignup}>
-            <GoPerson className={styles.iconMenu} />
-            <span>Login / SignUp</span>
-          </Link>
-        </div>
+        {menuOpen && (
+          <div className={styles.overlay} onClick={toggleMenu}></div>
+        )}
       </div>
     </header>
   );
