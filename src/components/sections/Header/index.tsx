7baa2ae5b09143b4ee557889e18c5c26a1449c62
ViniciusGR797 <loritoamarelo@outlook.com>
2024-08-22 22:33:28 -0300

    ✈feat: add overlay header

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -45,147 +45,152 @@ const Header: React.FC = () => {
   }, []);
 
   return (
-    <header className={styles.header}>
-      <RevealWrapper
-        origin="top"
-        delay={200}
-        duration={1000}
-        className={styles.contact}
-      >
-        <div className={styles.contactLeft}>
-          <p>(000) 999-898-999</p>
-          <span className={styles.separator}>|</span>
-          <p>info@trisog.com</p>
-        </div>
-        <div className={styles.contactRight}>
-          <a href="https://x.com" target="_blank" rel="noopener noreferrer">
-            <FaTwitter className={styles.iconSocialMedia} />
-          </a>
-          <a
-            href="https://linkedin.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedinIn className={styles.iconSocialMedia} />
-          </a>
-          <a
-            href="https://google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGoogle className={styles.iconSocialMedia} />
-          </a>
-          <a
-            href="https://pinterest.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaPinterestP className={styles.iconSocialMedia} />
-          </a>
-          <span className={styles.separator}>|</span>
-          <select className={styles.currencySelect}>
-            <option value="EUR">EUR</option>
-            <option value="USD">USD</option>
-            <option value="BRL">BRL</option>
-          </select>
-        </div>
-      </RevealWrapper>
-
-      <RevealWrapper
-        origin="top"
-        delay={200}
-        duration={1000}
-        className={styles.menu}
-      >
-        <Link href="/home">
-          <Image
-            src="/logo.svg"
-            alt="Logo"
-            width={120}
-            height={35}
-            priority={true}
-            className={styles.logo}
-          />
-        </Link>
-
-        <div className={styles.menuToggle} onClick={toggleMenu}>
-          {menuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
-        </div>
-
-        <nav className={`${styles.nav} ${menuOpen ? styles.menuOpen : ""}`}>
-          <ul className={styles.menuBar}>
-            <li className={router.pathname === "/home" ? styles.active : ""}>
-              <Link href="/home">Home</Link>
-            </li>
-            <li className={router.pathname === "/about" ? styles.active : ""}>
-              <Link href="/about">About</Link>
-            </li>
-            <li className={router.pathname === "/tuors" ? styles.active : ""}>
-              <Link href="/tuors">Tours</Link>
-            </li>
-            <li
-              className={
-                router.pathname === "/destination" ? styles.active : ""
-              }
-            >
-              <Link href="/destination">Destination</Link>
-            </li>
-            <li className={router.pathname === "/blog" ? styles.active : ""}>
-              <Link href="/blog">Blog</Link>
-            </li>
-            <li className={router.pathname === "/pages" ? styles.active : ""}>
-              <Link href="/pages">Pages</Link>
-            </li>
-            <li className={router.pathname === "/contact" ? styles.active : ""}>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-
-          <div className={styles.rightIcons}>
-            <Link href="/tuors">
-              <GoSearch className={styles.iconMenu} />
-            </Link>
-
-            {user ? (
-              <div
-                className={styles.userProfile}
-                onClick={() => setShowLogout(!showLogout)}
-              >
-                <Image
-                  src={user.photoUrl ? user.photoUrl : "/images/user.svg"}
-                  alt={
-                    user.firstName ? user.firstName : user.email?.split("@")[0]
-                  }
-                  width={35}
-                  height={35}
-                  className={styles.userPhoto}
-                />
-                <span>
-                  {user.firstName ? user.firstName : user.email?.split("@")[0]}
-                </span>
-                {showLogout && (
-                  <button
-                    onClick={handleLogout}
-                    className={styles.logoutButton}
-                  >
-                    Logout
-                  </button>
-                )}
-              </div>
-            ) : (
-              <Link href="/" className={styles.loginSignup}>
-                <GoPerson className={styles.iconMenu} />
-                <span>Login / SignUp</span>
-              </Link>
-            )}
+    <>
+      <header className={styles.header}>
+        <RevealWrapper
+          origin="top"
+          delay={200}
+          duration={1000}
+          className={styles.contact}
+        >
+          <div className={styles.contactLeft}>
+            <p>(000) 999-898-999</p>
+            <span className={styles.separator}>|</span>
+            <p>info@trisog.com</p>
           </div>
-        </nav>
+          <div className={styles.contactRight}>
+            <a href="https://x.com" target="_blank" rel="noopener noreferrer">
+              <FaTwitter className={styles.iconSocialMedia} />
+            </a>
+            <a
+              href="https://linkedin.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedinIn className={styles.iconSocialMedia} />
+            </a>
+            <a
+              href="https://google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGoogle className={styles.iconSocialMedia} />
+            </a>
+            <a
+              href="https://pinterest.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaPinterestP className={styles.iconSocialMedia} />
+            </a>
+            <span className={styles.separator}>|</span>
+            <select className={styles.currencySelect}>
+              <option value="EUR">EUR</option>
+              <option value="USD">USD</option>
+              <option value="BRL">BRL</option>
+            </select>
+          </div>
+        </RevealWrapper>
 
-        {menuOpen && (
-          <div className={styles.overlay} onClick={toggleMenu}></div>
-        )}
-      </RevealWrapper>
-    </header>
+        <RevealWrapper
+          origin="top"
+          delay={200}
+          duration={1000}
+          className={styles.menu}
+        >
+          <Link href="/home">
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={120}
+              height={35}
+              priority={true}
+              className={styles.logo}
+            />
+          </Link>
+
+          <div className={styles.menuToggle} onClick={toggleMenu}>
+            {menuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+          </div>
+
+          <nav className={`${styles.nav} ${menuOpen ? styles.menuOpen : ""}`}>
+            <ul className={styles.menuBar}>
+              <li className={router.pathname === "/home" ? styles.active : ""}>
+                <Link href="/home">Home</Link>
+              </li>
+              <li className={router.pathname === "/about" ? styles.active : ""}>
+                <Link href="/about">About</Link>
+              </li>
+              <li className={router.pathname === "/tuors" ? styles.active : ""}>
+                <Link href="/tuors">Tours</Link>
+              </li>
+              <li
+                className={
+                  router.pathname === "/destination" ? styles.active : ""
+                }
+              >
+                <Link href="/destination">Destination</Link>
+              </li>
+              <li className={router.pathname === "/blog" ? styles.active : ""}>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li className={router.pathname === "/pages" ? styles.active : ""}>
+                <Link href="/pages">Pages</Link>
+              </li>
+              <li
+                className={router.pathname === "/contact" ? styles.active : ""}
+              >
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+
+            <div className={styles.rightIcons}>
+              <Link href="/tuors">
+                <GoSearch className={styles.iconMenu} />
+              </Link>
+
+              {user ? (
+                <div
+                  className={styles.userProfile}
+                  onClick={() => setShowLogout(!showLogout)}
+                >
+                  <Image
+                    src={user.photoUrl ? user.photoUrl : "/images/user.svg"}
+                    alt={
+                      user.firstName
+                        ? user.firstName
+                        : user.email?.split("@")[0]
+                    }
+                    width={35}
+                    height={35}
+                    className={styles.userPhoto}
+                  />
+                  <span>
+                    {user.firstName
+                      ? user.firstName
+                      : user.email?.split("@")[0]}
+                  </span>
+                  {showLogout && (
+                    <button
+                      onClick={handleLogout}
+                      className={styles.logoutButton}
+                    >
+                      Logout
+                    </button>
+                  )}
+                </div>
+              ) : (
+                <Link href="/" className={styles.loginSignup}>
+                  <GoPerson className={styles.iconMenu} />
+                  <span>Login / SignUp</span>
+                </Link>
+              )}
+            </div>
+          </nav>
+        </RevealWrapper>
+      </header>
+      {menuOpen && <div className={styles.overlay} onClick={toggleMenu}></div>}
+    </>
   );
 };
 
