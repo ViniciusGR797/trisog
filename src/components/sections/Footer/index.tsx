e779c9da8a85258b91adfa97ff6ddb973923d18c
ViniciusGR797 <loritoamarelo@outlook.com>
2024-08-23 02:08:10 -0300

    🏕feat: send email

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaFacebookSquare, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 import { PiPaperPlaneTilt } from "react-icons/pi";
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import { RevealWrapper } from "next-reveal";
+import { toast } from "react-toastify";
 
 const Footer: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const validateField = (value: string) => {
+    return !/^\S+@\S+\.\S+$/.test(value) ? "Invalid email" : "";
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { value } = e.target;
+    setEmail(value);
+
+    const error = validateField(value);
+    setError(error);
+  };
+
+  useEffect(() => {
+    setIsFormValid(error === "" && email !== "");
+  }, [error, email]);
+
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        toast.success("Successfully subscribed! Check your inbox for updates.");
+        setEmail("");
+      } else {
+        toast.error("Something went wrong. Please try again later.");
+      }
+    } catch (error) {
+      toast.error("Something went wrong. Please try again later.");
+    }
+  };
+
   return (
     <footer className={styles.footer}>
       <RevealWrapper
@@ -26,16 +72,38 @@ const Footer: React.FC = () => {
               className={styles.logo}
             />
           </Link>
-          <div className={styles.contactInfo}>
+          <div className={`${styles.contactInfo} ${styles.listItems}`}>
             <p className={styles.needHelp}>Need any help?</p>
-            <p className={styles.phoneNumber}>
-              Call Us: <span>(888) 1234 5678</span>
-            </p>
+            <li>
+              <a
+                href="https://wa.me/88812345678?text=Hello%2C%20I%20am%20reaching%20out%20through%20your%20website%20to%20inquire%20about%20your%20travel%20packages.%20Could%20you%20please%20provide%20more%20information%20about%20the%20services%20you%20offer%20and%20how%20I%20can%20book%20a%20trip%3F%20Thank%20you."
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.phoneNumber}
+              >
+                Call Us: <span>(888) 1234 5678</span>
+              </a>
+            </li>
           </div>
 
-          <div className={styles.contactInfo}>
-            <p>Love Street, Muscat, Oman</p>
-            <p>example@trisog.com</p>
+          <div className={`${styles.contactInfo} ${styles.listItems}`}>
+            <li>
+              <a
+                href="https://www.google.com/maps/place/IN+LOVE+Mall+of+Oman/@23.5713592,58.4019331,17z/data=!4m6!3m5!1s0x3e91ff398e4c9b45:0x7650289911c719f6!8m2!3d23.5713592!4d58.404508!16s%2Fg%2F11tk90lg9z?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Love Street, Muscat, Oman
+              </a>
+            </li>
+
+            {/* <p>Love Street, Muscat, Oman</p> */}
+            <li>
+              <a href="mailto:example@trisog.com?subject=Travel%20Inquiry&body=Hello%2C%20I%20am%20contacting%20you%20through%20your%20website%20to%20inquire%20about%20travel%20packages.%20Could%20you%20please%20provide%20more%20details%20about%20your%20services%20and%20how%20I%20can%20book%20a%20trip%3F%20Thank%20you.">
+                example@trisog.com
+              </a>
+            </li>
+            <p></p>
           </div>
 
           <div className={styles.socialMedia}>
@@ -129,9 +197,14 @@ const Footer: React.FC = () => {
               type="email"
               id="newsletter"
               placeholder="Enter email..."
+              value={email}
+              onChange={handleChange}
             />
           </div>
-          <button type="button">Submit</button>
+          {error && <span className={styles.error}>{error}</span>}
+          <button type="button" onClick={handleClick} disabled={!isFormValid}>
+            Submit
+          </button>
           <p>&copy; 2023 Trisog All Right Reserved</p>
         </div>
       </RevealWrapper>
